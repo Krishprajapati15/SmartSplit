1c7366751b2154e28c72335379a684bd7ea6886a
Krishprajapati15 <prajapatikrish132005@gmail.com>
2025-05-05 08:46:13 +0530

    first commit

---
+++ drizzle.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config = defineConfig({
+  schema: "./database/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+    ssl: {
+      rejectUnauthorized: false, // Allow self-signed certificates
+    },
+  },
+});
+
+export default config;
